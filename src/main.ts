b7db0c2662ca74e11e1ff77daf6e9ba11f353756
Ignacio <ignacio.gibbs@gmail.com>
2024-07-12 13:30:41 -0300

    chore: Update dependencies and remove unused code

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger('MS Orders - Bootstrap');
